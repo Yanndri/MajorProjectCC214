0f9106c91e314163d2250abc27484eca944dc9c6
aaerial <aerialabella0@gmail.com>
2024-12-17 23:01:46 +0800

    Operation Search

---
+++ BookNode.java
@@ -0,0 +1,15 @@
+class BookNode {
+		private String title;
+		private String author;
+		private Node next;
+
+		public BookNode(String title, String author, Node next) {
+			this.title = title;
+			this.author = author;
+			this.next = next;
+		}
+
+		public BookNode(String title, String author) {
+			this(title, author, null);
+		}
+	} //end of BookNode class
